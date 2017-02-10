d24589c9cb03af9220dd96528e3b30bc1d92b5ab
Ryan Durrant <rddurrant@gmail.com>
2017-02-09 21:01:13 -0700

    updates the product feature

--- app.js
+++ app.js
@@ -14,10 +14,27 @@
 	) {
 		$scope.title = 'The Awesome Apple App';
 
-		$scope.featureProduct = {
-			name: 'Macbook Pro',
-			price: 3000
+		// $scope.featureProduct = {
+		// 	name: 'Macbook Pro',
+		// 	price: 3000,
+		// 	featured: true
+		// };
+
+		$scope.makeFeatured = function (name, price) {
+			$scope.featureProduct = {
+				name: name,
+				price: price
+			};
 		};
+
+//when the checkbox is clicked it will run makeFeatured
+		$scope.featureIt = function (product) {
+			product.featured = true;
+			console.log(product);
+			$scope.makeFeatured(product.name, product.price);
+		};
+
+
 
 		$scope.products = productsService.getProducts();
 
@@ -25,7 +42,7 @@
 			productsService.addProduct(
 				$scope.myInputName, $scope.myInputPrice
 			);
-		}
+		};
 	});
 
 	apple.service('productsService', function () {
@@ -33,30 +50,35 @@
 		var products = [
 			{
 				name: 'Macbook Pro 15 inch',
-				price: 3000
+				price: 3000,
+				featured: false
 			},
 			{
 				name: 'Macbook Pro 13 inch',
-				price: 1699.99
+				price: 1699.99,
+				featured: false
 			},
 			{
 				name: 'iPad Pro',
-				price: 999.99
+				price: 999.99,
+				featured: false
 			},
 			{
 				name: 'iPad',
-				price: 799.99
+				price: 799.99,
+				featured: false
 			},
 			{
 				name: 'iPhone',
-				price: 679.99
+				price: 679.99,
+				featured: false
 			}
 		];
 
 		this.getProducts = function () {
 			// check authentication
 			return products;
-		}
+		};
 
 		this.addProduct = function (name, price) {
 			console.log('this defective service function fired!');
@@ -64,7 +86,7 @@
 				name: name,
 				price: price
 			});
-		}
+		};
 	});
 
 	var ms = angular.module('microsoft', []);
@@ -75,8 +97,8 @@
 // we register a module with angular
 
 
-/* 
-	
+/*
+
 */
 // the angular object has functions lime module
-// 
+//
